dfba995b215622e7033bfd14c0c394e929407857
Max Henkel <max@maxhenkel.de>
2023-11-23 13:09:18 +0100

    Initial 1.20.3 port

--- src/main/java/de/maxhenkel/resourcepackchecker/ClientConfig.java
+++ src/main/java/de/maxhenkel/resourcepackchecker/ClientConfig.java
@@ -6,10 +6,8 @@ import de.maxhenkel.configbuilder.ConfigEntry;
 public class ClientConfig {
 
     public final ConfigEntry<Boolean> ignoreServerPacks;
-    public final ConfigEntry<Boolean> saveServerPacks;
 
     public ClientConfig(ConfigBuilder builder) {
         ignoreServerPacks = builder.booleanEntry("ignore_server_packs", false);
-        saveServerPacks = builder.booleanEntry("save_server_packs", false);
     }
 }
--- src/main/java/de/maxhenkel/resourcepackchecker/mixin/ClientCommonPacketListenerImplMixin.java
+++ src/main/java/de/maxhenkel/resourcepackchecker/mixin/ClientCommonPacketListenerImplMixin.java
@@ -5,10 +5,12 @@ import de.maxhenkel.resourcepackchecker.ResourcePackChecker;
 import de.maxhenkel.resourcepackchecker.ShaUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
-import net.minecraft.network.protocol.common.ClientboundResourcePackPacket;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.common.ClientboundResourcePackPushPacket;
 import net.minecraft.network.protocol.common.ServerboundResourcePackPacket;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.repository.Pack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,17 +23,21 @@ import java.util.Collection;
 @Mixin(ClientCommonPacketListenerImpl.class)
 public abstract class ClientCommonPacketListenerImplMixin {
 
-    @Inject(method = "handleResourcePack", at = @At("HEAD"), cancellable = true)
-    private void setServerPack(ClientboundResourcePackPacket clientboundResourcePackPacket, CallbackInfo ci) {
+    @Shadow
+    @Final
+    protected Connection connection;
+
+    @Inject(method = "handleResourcePackPush", at = @At("HEAD"), cancellable = true)
+    private void setServerPack(ClientboundResourcePackPushPacket packet, CallbackInfo ci) {
         if (ResourcePackChecker.CLIENT_CONFIG.ignoreServerPacks.get()) {
-            ResourcePackChecker.LOGGER.info("Ignored server resource pack '{}', required={}", clientboundResourcePackPacket.getUrl(), clientboundResourcePackPacket.isRequired());
+            ResourcePackChecker.LOGGER.info("Ignored server resource pack '{}', required={}", packet.url(), packet.required());
             ci.cancel();
         }
     }
 
-    @Inject(method = "handleResourcePack", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/common/ClientboundResourcePackPacket;isRequired()Z", shift = At.Shift.AFTER), cancellable = true)
-    private void handleResourcePack(ClientboundResourcePackPacket packet, CallbackInfo ci) {
-        if (packet.getHash().isBlank()) {
+    @Inject(method = "handleResourcePackPush", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/common/ClientboundResourcePackPushPacket;required()Z", shift = At.Shift.AFTER), cancellable = true)
+    private void handleResourcePack(ClientboundResourcePackPushPacket packet, CallbackInfo ci) {
+        if (packet.hash().isBlank()) {
             return;
         }
         Collection<Pack> selectedPacks = Minecraft.getInstance().getResourcePackRepository().getSelectedPacks();
@@ -40,16 +46,13 @@ public abstract class ClientCommonPacketListenerImplMixin {
             if (resources instanceof IFilePackResource) {
                 File f = ((IFilePackResource) resources).getFile();
                 String packSha1 = ShaUtils.getSha1(f);
-                if (packSha1.equals(packet.getHash())) {
-                    send(ServerboundResourcePackPacket.Action.ACCEPTED);
+                if (packSha1.equals(packet.hash())) {
+                    connection.send(new ServerboundResourcePackPacket(packet.id(), ServerboundResourcePackPacket.Action.ACCEPTED));
                     ci.cancel();
                     break;
                 }
             }
         }
     }
-
-    @Shadow
-    protected abstract void send(ServerboundResourcePackPacket.Action action);
 
 }
